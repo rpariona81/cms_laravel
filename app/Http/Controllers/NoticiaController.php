cfa72a0318a248c58863a98333edcf9902dfff83
rpariona81 <ronald@RPARIONA-PCDELL.localdomain>
2022-11-29 21:29:03 -0500

    avance de noticias

--- app/Http/Controllers/NoticiaController.php
+++ app/Http/Controllers/NoticiaController.php
@@ -2,9 +2,44 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Noticia;
 use Illuminate\Http\Request;
 
 class NoticiaController extends Controller
 {
     //
+    public function __construct()
+    {
+        /**
+         * Asigno el middleware auth al controlador
+         * de modo que sea necesario estar al menos autenticado
+         */
+        $this->middleware('auth');
+    }
+
+    /**
+     * Mostrar un listado de elementos
+     * 
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //Obtengo todas las noticias ordenadas por fecha más reciente
+        $rowset = Noticia::orderBy('fecha','DESC')->get();
+
+        return view('admin.noticias.index',[
+            'rowset' => $rowset,
+        ]);
+    }
+
+    /**
+     * Mostrar el formulario para crear un nuevo elemento
+     * 
+     * @return \Illuminate\Http\Response
+     */
+    public function crear()
+    {
+        
+    }
+    
 }
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AppController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\NoticiaController;
 use App\Http\Controllers\UsuarioController;
 
 /*
@@ -40,6 +41,17 @@ Route::get('admin/usuarios/actualizar/{id}', [UsuarioController::class, 'actuali
 Route::get('admin/usuarios/activar/{id}', [UsuarioController::class, 'activar'])->middleware('role:usuarios');
 Route::get('admin/usuarios/borrar/{id}', [UsuarioController::class, 'borrar'])->middleware('role:usuarios');
 
+//Back-end noticias
+Route::get('admin', [AdminController::class, 'index'])->name('admin');
+Route::get('admin/noticias', [NoticiaController::class, 'index'])->middleware('role:noticias');
+Route::get('admin/noticias/crear', [NoticiaController::class, 'crear'])->middleware('role:noticias');
+Route::get('admin/noticias/guardar', [NoticiaController::class, 'guardar'])->middleware('role:noticias');
+Route::get('admin/noticias/editar/{id}', [NoticiaController::class, 'editar'])->middleware('role:usunoticiasarios');
+Route::get('admin/noticias/actualizar/{id}', [NoticiaController::class, 'actualizar'])->middleware('role:noticias');
+Route::get('admin/noticias/activar/{id}', [NoticiaController::class, 'activar'])->middleware('role:noticias');
+Route::get('admin/noticias/home/{id}', [NoticiaController::class, 'home'])->middleware('role:noticias');
+Route::get('admin/noticias/borrar/{id}', [NoticiaController::class, 'borrar'])->middleware('role:noticias');
+
 //Auth
 Route::get('acceder', [AuthController::class, 'acceder'])->name('acceder');
 Route::post('autenticar', [AuthController::class, 'autenticar'])->name('autenticar');
@@ -48,3 +60,5 @@ Route::post('registrarse', [AuthController::class, 'registrarse'])->name('regist
 Route::post('salir', [AuthController::class, 'salir'])->name('salir');
 
 
+//Ruta por defecto (si no encuentra otra antes)
+Route::any('{query}', function() { return redirect('/'); })->where('query', '.*');
